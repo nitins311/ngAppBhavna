7c8848977dedc1c865af297e6f31615dd586a2b3
Nitin Singh <Nitin.Singh@Yardi.com>
2019-01-25 13:51:13 +0530

    Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OrderConfirmationComponent } from './lazyModules/coupons/order-confirmation/order-confirmation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'order-confirmation', component:OrderConfirmationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrderConfirmationComponent } from './lazyModules/coupons/order-confirmation/order-confirmation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderConfirmationComponent
   ],
   imports: [
     BrowserModule,
